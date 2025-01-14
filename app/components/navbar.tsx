84df593e9e25a682a4031a00c5ea88ac6b22fa00
vamshichi <chvamshi03@gmail.com>
2025-01-14 17:14:28 +0530

    changed content

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react"
 import Link from "next/link"
-import Image from "next/image"
+// import Image from "next/image"
 import { usePathname } from "next/navigation"
 import { Menu, X } from 'lucide-react'
 import { motion, AnimatePresence } from "framer-motion"
