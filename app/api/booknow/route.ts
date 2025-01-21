a959f2c0f3a78bcabc0698d517ff5e1d809ed025
vamshichi <chvamshi03@gmail.com>
2025-01-22 00:11:13 +0530

    Your commit message

--- app/api/booknow/route.ts
+++ app/api/booknow/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const {
