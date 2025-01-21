8774c51e446c1a29c422607e5172bd8ac7b86a56
vamshichi <chvamshi03@gmail.com>
2025-01-22 00:13:59 +0530

    Your commit message

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -75,7 +75,7 @@ export function Navbar() {
             >
               <Link 
                 href="/booknow"
-                className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
+                className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-blue-800 bg-primary hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
               >
                 Book Now
               </Link>
